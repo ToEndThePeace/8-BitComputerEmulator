6023565e30faf7bfddf5c4abbc673325a5468a04
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-08-18 13:22:59 -0700

    Implemented hashtables for function mapping to keep the run program clean. Implemented new functions on the CPU.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -13,12 +13,24 @@ class CPU:
         self.pc = 0
         self.mar = 0
         self.mdr = 0
+        self.running = True
 
-        # instruction definitions
-        self.HLT = 1
-        self.LDI = 130
-        self.PRN = 71
-        self.MUL = 162
+        # set comparison flags default to 0 (last 3 bits are L, G, E)
+        self.FL = 0b00000000
+
+        # alu operation table
+        self.alutable = {}
+        self.alutable[0b0] = "ADD"
+        self.alutable[0b10] = "MUL"
+        self.alutable[0b1000] = "AND"
+        self.alutable[0b0111] = "CMP"
+        self.alutable[0b110] = "DEC"
+
+        # non-alu instruction definitions
+        self.branchtable = {}
+        self.branchtable[0b1] = self.HLT
+        self.branchtable[0b10] = self.LDI
+        self.branchtable[0b111] = self.PRN
 
     def ram_read(self):
         return self.reg[self.mar]
@@ -28,33 +40,30 @@ class CPU:
 
     def load(self, program):
         """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-    def alu(self, op, reg_a, reg_b):
+    def alu(self, op, reg_a=None, reg_b=None):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "DEC":
+            self.reg[reg_a] -= 1
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "AND":
+            self.reg[reg_a] &= self.reg[reg_b]
+        elif op == "CMP":
+            diff = self.reg[reg_a] - self.reg[reg_b]
+            if diff < 0:
+                self.FL = 0b100
+            elif diff > 0:
+                self.FL = 0b10
+            else:
+                self.FL = 0b1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -78,39 +87,49 @@ class CPU:
 
         print()
 
+    def HLT(self):
+        self.running = False
+
+    def LDI(self, reg_index, value):
+        self.mar = reg_index
+        self.mdr = value
+        self.ram_write()
+
+    def PRN(self, reg_index):
+        self.mar = reg_index
+        print(self.ram_read())
+
     def run(self):
         """Run the CPU."""
-        running = True
-
-        while running:
+        while self.running:
+            # grab the IR
             ir = self.ram[self.pc]
-            # print(ir)
-            if ir == self.HLT:
-                # print("HLT")
-                running = False
-                self.pc += 1
-            elif ir == self.LDI:
-                # print("LDI")
-                # load the arguments into the proper registers
-                self.mar = self.ram[self.pc + 1]
-                self.mdr = self.ram[self.pc + 2]
-                # update CPU with the stored data
-                self.ram_write()
-                # increment our program counter
-                self.pc += 3
-            elif ir == self.PRN:
-                # print("PRN")
-                # load the argument into the register
-                self.mar = self.ram[self.pc + 1]
-                # print out the value at the register
-                print(self.ram_read())
-                self.pc += 2
-            elif ir == self.MUL:
-                a = self.ram[self.pc + 1]
-                b = self.ram[self.pc + 2]
-                self.alu("MUL", a, b)
 
-                self.pc += 3
-            else:
-                print("Invalid function call. Exiting...")
-                running = False
+            # pull out the relevant data to be processed
+            instruction_code = ir & 0b1111
+            setsPC = (ir >> 4) & 0b1
+            isALU = (ir >> 5) & 0b1
+            num_args = ir >> 6
+
+            # if it's an alu function, process it through the alu
+            if num_args == 0:
+                if isALU:
+                    self.alu(self.alutable[instruction_code])
+                else:
+                    self.branchtable[instruction_code]()
+            elif num_args == 1:
+                arg_a = self.ram[self.pc + 1]
+                if isALU:
+                    self.alu(self.alutable[instruction_code], arg_a)
+                else:
+                    self.branchtable[instruction_code](arg_a)
+            elif num_args == 2:
+                arg_a = self.ram[self.pc + 1]
+                arg_b = self.ram[self.pc + 2]
+                if isALU:
+                    self.alu(self.alutable[instruction_code], arg_a, arg_b)
+                else:
+                    self.branchtable[instruction_code](arg_a, arg_b)
+
+            # after any process, increment the pc
+            self.pc += num_args + 1
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,14 +7,25 @@ from cpu import *
 
 cpu = CPU()
 
-if len(sys.argv) > 1:
-    program = []
+if len(sys.argv) != 2:
+    print("usage: ls8.py 'programname'\nExiting...")
+    sys.exit(1)
+    
+program = []
+try:
     with open(sys.argv[1]) as f:
         for line in f:
             x = line.split()
-            if len(x) > 0 and x[0] != "#":
+            if len(x) == 0 or x[0] == "#":
+                continue
+
+            try:
                 program.append(int(x[0], 2))
+            except ValueError:
+                print(f"Invalid value: {x[0]}")
+    # print(program)
     cpu.load(program)
     cpu.run()
-else:
-    print("No program specified! Exiting...")
+except FileNotFoundError:
+    print(f"Couldn't open file: {sys.argv[1]}")
+    sys.exit(2)
