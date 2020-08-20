7b0a55e2a75ed060e7c19412e0993de7fbd9cabb
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-08-19 22:57:23 -0700

    Added skeleton for the rest of the CPU methods and implemented ability to call to and return from subroutines in program execution. Next step is implementing interrupts.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,10 +1,8 @@
 """
 CPU functionality.
 
-skipped so far:
+methods needed to implement:
 CALL
-INT
-IRET
 JEQ
 JGE
 JGT
@@ -12,10 +10,7 @@ JLE
 JMP
 JNE
 RET
-
-
-Maybe I can do this?
-ST
+INT
 """
 
 import sys
@@ -46,31 +41,45 @@ class CPU:
 
         # alu operation table
         self.alutable = {}
-        self.alutable[0b0] = "ADD"
-        self.alutable[0b1] = "SUB"
-        self.alutable[0b10] = "MUL"
-        self.alutable[0b11] = "DIV"
+        self.alutable[0b0000] = "ADD"
+        self.alutable[0b0001] = "SUB"
+        self.alutable[0b0010] = "MUL"
+        self.alutable[0b0011] = "DIV"
+        self.alutable[0b0100] = "MOD"
+        self.alutable[0b0101] = "INC"
+        self.alutable[0b0110] = "DEC"
         self.alutable[0b0111] = "CMP"
-        self.alutable[0b110] = "DEC"
-        self.alutable[0b101] = "INC"
-        self.alutable[0b100] = "MOD"
         self.alutable[0b1000] = "AND"
-        self.alutable[0b1010] = "OR"
         self.alutable[0b1001] = "NOT"
+        self.alutable[0b1010] = "OR"
         self.alutable[0b1011] = "XOR"
         self.alutable[0b1100] = "SHL"
         self.alutable[0b1101] = "SHR"
 
+        # jump functions
+        self.jumptable = {}
+        self.jumptable[0b0000] = self.CALL
+        self.jumptable[0b0001] = self.RET
+        self.jumptable[0b0010] = self.INT
+        self.jumptable[0b0011] = self.IRET
+        self.jumptable[0b0100] = self.JMP
+        self.jumptable[0b0101] = self.JEQ
+        self.jumptable[0b0110] = self.JNE
+        self.jumptable[0b0111] = self.JGT
+        self.jumptable[0b1000] = self.JLT
+        self.jumptable[0b1001] = self.JLE
+        self.jumptable[0b1010] = self.JGE
+
         # non-alu instruction definitions
         self.branchtable = {}
-        self.branchtable[0b1] = self.HLT
-        self.branchtable[0b10] = self.LDI
-        self.branchtable[0b11] = self.LD
-        self.branchtable[0b100] = self.ST
-        self.branchtable[0b111] = self.PRN
-        self.branchtable[0b1000] = self.PRA
-        self.branchtable[0b0110] = self.POP
+        self.branchtable[0b0001] = self.HLT
+        self.branchtable[0b0010] = self.LDI
+        self.branchtable[0b0011] = self.LD
+        self.branchtable[0b0100] = self.ST
         self.branchtable[0b0101] = self.PUSH
+        self.branchtable[0b0110] = self.POP
+        self.branchtable[0b0111] = self.PRN
+        self.branchtable[0b1000] = self.PRA
 
     def ram_read(self):
         return self.reg[self.mar]
@@ -190,10 +199,72 @@ class CPU:
         self.mar = reg_index
         print(chr(self.ram_read()))
 
+    # Jump Methods
+    def JMP(self, reg_index):
+        self.mar = reg_index
+        value = ram_read()
+        self.pc = value
+
+    def CALL(self, reg_index):
+        # push the next instruction's index onto the stack
+        next_index = self.pc + 2
+        self.reg[7] -= 1
+        self.ram[self.reg[7]] = next_index
+        self.pc = self.reg[reg_index]
+
+    def RET(self):
+        # print("b4: ", self.pc)
+        self.pc = self.ram[self.reg[7]]
+        # print("af: ", self.pc)
+        self.reg[7] += 1
+
+    def JEQ(self):
+        pass
+
+    def JGE(self):
+        pass
+
+    def JGT(self):
+        pass
+
+    def JLT(self):
+        pass
+
+    def JLE(self):
+        pass
+
+    def JMP(self):
+        pass
+
+    def JNE(self):
+        pass
+
+    def INT(self):
+        pass
+
+    def IRET(self):
+        # pop registers 6-0 off the stack in that order
+        for i in range(7):
+            self.mar = 6 - i
+            self.mdr = self.POP()
+            self.ram_write()
+        self.FL = self.POP()
+        self.pc = self.POP()
+
     def run(self):
-        """Run the CPU."""
+        # timer interrupt
+
+        # interrupt handling
+        # masked_interrupts = self.reg[5] & self.reg[6]
+        # for i in range(8):
+        #     interrupted = ((masked_interrupts >> i) & 1) == 1
+        #     if interrupted:
+        #         pass
+
+        # instruction fetch
         while self.running:
             # grab the IR
+            # print(self.pc)
             ir = self.ram[self.pc]
             # print(ir)
 
@@ -205,16 +276,22 @@ class CPU:
             # print(instruction_code, num_args)
 
             # if it's an alu function, process it through the alu
+            # if it's a jump function, process accordingly
             if num_args == 0:
                 if isALU:
                     self.alu(self.alutable[instruction_code])
+                elif setsPC:
+                    self.jumptable[instruction_code]()
                 else:
                     if instruction_code != 0:
+                        # skips NOP lines in program while still incrementing pc
                         self.branchtable[instruction_code]()
             elif num_args == 1:
                 arg_a = self.ram[self.pc + 1]
                 if isALU:
                     self.alu(self.alutable[instruction_code], arg_a)
+                elif setsPC:
+                    self.jumptable[instruction_code](arg_a)
                 else:
                     self.branchtable[instruction_code](arg_a)
             elif num_args == 2:
@@ -225,5 +302,6 @@ class CPU:
                 else:
                     self.branchtable[instruction_code](arg_a, arg_b)
 
-            # after any process, increment the pc
-            self.pc += num_args + 1
+            # after a process that doesn't set PC, increment the pc
+            if not setsPC:
+                self.pc += num_args + 1
---
+++ ls8/testing.py
@@ -0,0 +1,10 @@
+# from datetime import datetime
+# x = datetime.now()
+
+# input()
+# y = datetime.now()
+
+# print((y - x).seconds)
+
+for i in range(7):
+    print(6 - i)
